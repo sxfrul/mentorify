d1dcfc3c8dd774081ac5fdfeecb6118b6d2714c2
sxfrul <44408869+sxfrul@users.noreply.github.com>
2024-12-07 14:46:05 +0800

    + commit

--- llamaflask.py
+++ llamaflask.py
@@ -66,4 +66,4 @@ def stream_chat():
     return Response(generate_response_stream(user_message), content_type='text/event-stream')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
